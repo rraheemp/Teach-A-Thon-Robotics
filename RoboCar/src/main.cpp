b8dd73f2e05c84833749c8f2736394104469ebab
rraheemp <89361408+rraheemp@users.noreply.github.com>
2023-12-13 14:02:28 -0500

    Add files via upload

--- RoboCar/src/main.cpp
+++ RoboCar/src/main.cpp
@@ -7,7 +7,7 @@
 #define USER_SCL 22
 
 #include <Wire.h>
-// #include <VL53L0X.h>
+#include <VL53L0X.h>
 #include <SparkFun_TB6612.h>
 
 /*------------------------------------------------------------
@@ -50,7 +50,22 @@ Motor motor1 = Motor(MOTOR1_A, MOTOR1_B, MOTOR1_PWM, offsetA, STBY);
 Motor motor2 = Motor(MOTOR2_C, MOTOR2_D, MOTOR2_PWM, offsetB, STBY);
 
 // //Distance Sensor
-// VL53L0X VL53;
+VL53L0X sensor;
+
+// Uncomment this line to use long range mode. This
+// increases the sensitivity of the sensor and extends its
+// potential range, but increases the likelihood of getting
+// an inaccurate reading because of reflections from objects
+// other than the intended target. It works best in dark
+// conditions.
+
+// #define LONG_RANGE
+
+// Uncomment ONE of these two lines to get
+// - higher speed at the cost of lower accuracy OR
+// - higher accuracy at the cost of lower speed
+
+#define HIGH_SPEED
 // #define HIGH_ACCURACY
 
 /*------------------------------------------------------------
@@ -106,9 +121,41 @@ void setup()
 
   Serial.begin(115200);      // make sure your Serial Monitor is also set at this baud rate.
   Dabble.begin("RoboCart1"); // set bluetooth name of your device
+
   digitalWrite(LED, LOW);
   digitalWrite(REAR_LEFT_LED, LOW);
   digitalWrite(REAR_RIGHT_LED, LOW);
+  
+
+  /*------------------------------------------------------------
+  ----------------------Speed Constants--------------------------
+  --------------------------------------------------------------*/
+  Wire.begin(22,21);
+
+  sensor.setTimeout(200);
+  if (!sensor.init())
+  {
+    Serial.println("Failed to detect and initialize sensor!");
+    while (1)
+    {
+    }
+  }
+
+  #if defined LONG_RANGE
+  // lower the return signal rate limit (default is 0.25 MCPS)
+  sensor.setSignalRateLimit(0.1);
+  // increase laser pulse periods (defaults are 14 and 10 PCLKs)
+  sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18);
+  sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14);
+  #endif
+
+  #if defined HIGH_SPEED
+  // reduce timing budget to 20 ms (default is about 33 ms)
+  sensor.setMeasurementTimingBudget(20000);
+  #elif defined HIGH_ACCURACY
+  // increase timing budget to 200 ms
+  sensor.setMeasurementTimingBudget(200000);
+  #endif
 
   FlashBlueLed(10);
 }
@@ -116,6 +163,14 @@ void setup()
 void loop()
 {
   Dabble.processInput();
+
+  Serial.print(sensor.readRangeSingleMillimeters());
+  if (sensor.timeoutOccurred())
+  {
+    Serial.print(" TIMEOUT");
+  }
+
+  Serial.println();
 
   if (GamePad.isSelectPressed())
   {
@@ -156,7 +211,7 @@ void loop()
     // Brake lights
     digitalWrite(REAR_LEFT_LED, LOW);
     digitalWrite(REAR_RIGHT_LED, LOW);
-    nonBlockingDelay(10); // non-blocking delay
+    nonBlockingDelay(1); // non-blocking delay
 
     Serial.println("Idling. Brakes");
   }
@@ -170,7 +225,7 @@ void loop()
     // Brake lights
     digitalWrite(REAR_LEFT_LED, HIGH);
     digitalWrite(REAR_RIGHT_LED, HIGH);
-    nonBlockingDelay(10); // non-blocking delay
+    nonBlockingDelay(1); // non-blocking delay
 
     Serial.println("Down Pressed. Reversing...");
   }
@@ -183,7 +238,7 @@ void loop()
     // Brake lights
     digitalWrite(REAR_LEFT_LED, HIGH);
     digitalWrite(REAR_RIGHT_LED, LOW);
-    nonBlockingDelay(10); // non-blocking delay
+    nonBlockingDelay(1); // non-blocking delay
 
     Serial.println("Left Pressed. Turning Left...");
   }
@@ -196,7 +251,7 @@ void loop()
     // Brake lights
     digitalWrite(REAR_LEFT_LED, LOW);
     digitalWrite(REAR_RIGHT_LED, HIGH);
-    nonBlockingDelay(10); // non-blocking delay
+    nonBlockingDelay(1); // non-blocking delay
 
     Serial.println("Left Pressed. Turning Right...");
   }
@@ -211,5 +266,5 @@ void loop()
     digitalWrite(REAR_RIGHT_LED, HIGH);
     Serial.println("Idling. Brakes");
   }
-  nonBlockingDelay(10); // non-blocking delay
+  nonBlockingDelay(1); // non-blocking delay
 }
